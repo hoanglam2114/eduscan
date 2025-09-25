facf26a9427b4e199c25dfa47278b9004f3d87da
hoanglam2114 <lamdhhe186575@fpt.edu.vn>
2025-09-25 08:48:07 +0700

    project setup

---
+++ src/test/java/fpt/eduscan/EduScanAiApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.eduscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduScanAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
